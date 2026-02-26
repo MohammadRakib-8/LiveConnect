ad9e963d94ce6e78eaf5dea12affe1887869615b
Moahmmad Rakib <mohammadrakib224@gmail.com>
2026-02-26 14:24:49 +0600

    Delete coe updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,7 +49,7 @@ class User extends Authenticatable
 
     public function conversations()
     {
-        return $this->hasMany(Conversation::class,'sender_id')->orWhere('receiver_id',$this->id);
+        return $this->hasMany(Conversation::class,'sender_id')->orWhere('receiver_id',$this->id)->whereNotDeleted();
     }
 
      public function receivesBroadcastNotificationsOn(): string
